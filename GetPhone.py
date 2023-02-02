073edc900b75e8c563ba8c91a2fb3c20c068668f
Makar85 <Levitin85@yandex.ru>
2023-02-02 22:05:06 +0300

    Added voice Assistant

--- GetPhone.py
+++ GetPhone.py
@@ -1,7 +1,24 @@
 from bs4 import BeautifulSoup
+import gtts
+from playsound import playsound
+import pyttsx3
 import requests
+# make a request to google to get synthesis 
+t1 = gtts.gTTS("Привет")
+# initialize Text-to-speech engine
+engine = pyttsx3.init()
+# convert this text to speech
+text = "Приветствую тебя человек, введи номер телефона чтобы узнать все тайны"
+engine.say(text)
+#play the speech
+engine.runAndWait()
+#get details of speacking rate
+rate = engine.getProperty("rate")
 datas={'number': 'def'}
 tel = input("Введите телефон: ")
+text1 = "Прекрасный номер, вот информация которую вы искали"
+engine.say(text1)
+engine.runAndWait()
 datas['number'] = tel
 url = "https://www.kody.su/check-tel#text"
 ses = requests.Session()
